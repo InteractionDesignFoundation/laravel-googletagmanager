4250563308a0aacf56b1c69491d2d5b75ab0b5cc
Ekin <ocalan.ekin@gmail.com>
2017-04-28 01:41:15 +0300

    Do not escape html characters when putting environment

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -7,12 +7,12 @@ dataLayer.push({!! $item->toJson() !!});
 @endforeach
 </script>
 @if($environmentEnabled)
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id={{ $id . $environmentAddition }}"
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id={!! $id . $environmentAddition !!}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'//www.googletagmanager.com/gtm.js?id='+i+dl+'{{$environmentAddition}}';f.parentNode.insertBefore(j,f);
+'//www.googletagmanager.com/gtm.js?id='+i+dl+'{!! $environmentAddition !!}';f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','{{ $id }}');</script>
 @else
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id={{ $id }}"
