bfeb95651892ba838aeb0f9c68f2f747988f9c93
Alies Lapatsin <web.lapateen@gmail.com>
2020-10-27 11:18:26 +0300

    Return back env support

--- resources/views/body.blade.php
+++ resources/views/body.blade.php
@@ -1,4 +1,9 @@
 @if($enabled)
+@if($environmentEnabled)
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id={!! $id . $environmentAddition !!}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+@else
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ $id }}"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+@endif
 @endif
--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -6,9 +6,17 @@ dataLayer = [{!! $dataLayer->toJson() !!}];
 dataLayer.push({!! $item->toJson() !!});
 @endforeach
 </script>
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','{{ $id }}');</script>
+    @if($environmentEnabled)
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                '//www.googletagmanager.com/gtm.js?id='+i+dl+'{!! $environmentAddition !!}';f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','{{ $id }}');</script>
+    @else
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','{{ $id }}');</script>
+    @endif
 @endif
